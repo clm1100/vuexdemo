126166d24f476e1453821cbbdf3ab85de3f18757
clm110 <275298711@qq.com>
2019-04-22 14:12:38 +0800

    初始化拆分组件

---
+++ 购物车/components/carheader.js
@@ -0,0 +1,11 @@
+Vue.component("Carheader",{
+    template:`
+    <div class="car-header">
+        <div class="w">
+            <div class="car-logo">
+                <img src="img/logo.png" alt=""> <b>购物车</b>
+            </div>
+        </div>
+    </div>
+    `
+})
---
+++ 购物车/js/car3.js
@@ -0,0 +1,12 @@
+
+new Vue({
+    el:"#app",
+    template:`
+    <div>
+        <Start />
+        <Carheader/>
+        <Carcontainer />
+        <Footer />
+    <div>
+    `
+})
